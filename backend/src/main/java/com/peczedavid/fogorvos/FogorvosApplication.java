1627e24fc32c60421b3c92d939bb5f454a57bce6
Pecze Dávid <peczedavid@gmail.com>
2022-11-04 16:16:19 +0100

    todos

--- backend/src/main/java/com/peczedavid/fogorvos/FogorvosApplication.java
+++ backend/src/main/java/com/peczedavid/fogorvos/FogorvosApplication.java
@@ -6,6 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 // TODO: kérdés: service-ben exception, ami visszatér a controllerben egy responsebody-val (ProcessInstanceController.setVariable() -nél kéne)
 
+// TODO: backend: spring security login rendszer, jwt, passwordencoder
+//       frontend: state management erre a login rendszerre
+
 @EnableProcessApplication
 @SpringBootApplication
 public class FogorvosApplication {
