8f058751436a50f454d93a0361527d760f5ffa71
Pecze Dávid <peczedavid@gmail.com>
2022-11-09 10:19:41 +0100

    Task not found handling + Colored logging

--- backend/src/main/java/com/peczedavid/fogorvos/service/TaskServiceCustom.java
+++ backend/src/main/java/com/peczedavid/fogorvos/service/TaskServiceCustom.java
@@ -1,10 +1,13 @@
 package com.peczedavid.fogorvos.service;
 
+import com.peczedavid.fogorvos.model.network.MessageResponse;
 import com.peczedavid.fogorvos.model.task.generic.TaskPayload;
 import org.camunda.bpm.engine.RuntimeService;
 import org.camunda.bpm.engine.TaskService;
 import org.camunda.bpm.engine.rest.dto.runtime.VariableInstanceDto;
 import org.camunda.bpm.engine.task.Task;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,15 +19,20 @@ import java.util.stream.Collectors;
 @Service
 public class TaskServiceCustom {
 
+    private static final Logger logger = LoggerFactory.getLogger(TaskServiceCustom.class);
+
     @Autowired
     private TaskService taskService;
 
     @Autowired
     private RuntimeService runtimeService;
 
-    public ResponseEntity<TaskPayload> getTask(String id) {
-        Task task = taskService.createTaskQuery().taskId(id).list().get(0);
-        // TODO: task not found
+    public ResponseEntity<?> getTask(String id) {
+        Task task = taskService.createTaskQuery().taskId(id).singleResult();
+        if(task == null) {
+            logger.error("Task " + id + " not found");
+            return new ResponseEntity<>(new MessageResponse("Task " + id + " not found!"), HttpStatus.NOT_FOUND);
+        }
         List<VariableInstanceDto> taskVariables = runtimeService
                 .createVariableInstanceQuery()
                 .processInstanceIdIn(task.getProcessInstanceId())
